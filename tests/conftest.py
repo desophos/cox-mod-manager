d2c31070e9d6daf1a051b16020464407596a9a5d
desophos <1887450+desophos@users.noreply.github.com>
2020-08-03 23:23:46 -0500

    testmods & other convenient fixtures
    Includes programmatic creation of test zipfiles

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,54 @@
+import json
 import shutil
 from pathlib import Path
+from typing import Dict, Iterator
+from zipfile import ZipFile
 
 import pytest
-from cox_mod_manager.context import context
+from cox_mod_manager.context import ModsContext, context
+from cox_mod_manager.mod import Mod
 
 
 @pytest.fixture(scope="session")
-def ctx(tmp_path_factory):
-    # TODO: create test mods programmatically
-    mods_dir = tmp_path_factory.mktemp("mods")
-    install_dir = tmp_path_factory.mktemp("install")
-    shutil.copy(Path("tests/mods/testmod.zip"), mods_dir)
-    shutil.copy(Path("tests/mods/testmod2.zip"), mods_dir)
+def mods_dir(tmp_path_factory) -> Path:
+    return tmp_path_factory.mktemp("mods")
+
+
+@pytest.fixture(scope="session")
+def install_dir(tmp_path_factory) -> Path:
+    return tmp_path_factory.mktemp("install")
+
+
+@pytest.fixture(scope="session")
+def testmods(mods_dir, install_dir) -> Dict[str, Mod]:
+    mods = {
+        "Test Mod 1": Mod(
+            "testmod.zip",
+            [Path("data/test/test.txt"), Path("data/test/test2.txt")],
+            install_dir,
+            {"name": "Test Mod 1"},
+        ),
+        "Test Mod 2": Mod(
+            "testmod2.zip",
+            [Path("data/test/test2/test2.txt"), Path("data/test/test3/test3.txt")],
+            install_dir,
+            {"name": "Test Mod 2"},
+        ),
+    }
+
+    for mod in mods.values():
+        with ZipFile(mods_dir / mod.filename, "w") as zf:
+            for f in mod.files:
+                # f.parent.mkdir(parents=True)
+                zf.writestr(str(f), "")
+            zf.writestr("info.json", json.dumps(mod.info))
+
+    return mods
+
+
+@pytest.fixture(scope="session")
+def ctx(testmods, tmp_path_factory, mods_dir, install_dir) -> ModsContext:
     with context(
         root=tmp_path_factory.getbasetemp(), mods_dir=mods_dir, install_dir=install_dir
     ) as ctx:
-        yield ctx
-    # TODO: delete temp files
+        return ctx
--- tests/test_context.py
+++ tests/test_context.py
@@ -1,20 +1,11 @@
-from pathlib import Path
-
-testmods = {
-    "Test Mod 1": [Path("data/test/test.txt"), Path("data/test/test2.txt")],
-    "Test Mod 2": [
-        Path("data/test/test2/test2.txt"),
-        Path("data/test/test3/test3.txt"),
-    ],
-}
 
 
-def test_discover(ctx):
+
+def test_discover(ctx, testmods):
     assert sorted(ctx.mods.keys()) == sorted(testmods.keys())
-    for name, paths in testmods.items():
-        mod = ctx.mods[name]
-        for path in paths:
-            assert path in mod.files
+    for name, mod in testmods.items():
+        for path in mod.files:
+            assert path in ctx.mods[name].files
 
 
 # TODO: enter/exit tests
