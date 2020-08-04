2094558d48c8846e7ca6a12460ff329331466cf8
desophos <1887450+desophos@users.noreply.github.com>
2020-08-03 23:26:33 -0500

    enter/exit context tests

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,4 +1,7 @@
+import json
 
+from cox_mod_manager.context import context
+from cox_mod_manager.json import ModEncoder, decode_mod
 
 
 def test_discover(ctx, testmods):
@@ -8,4 +11,26 @@ def test_discover(ctx, testmods):
             assert path in ctx.mods[name].files
 
 
-# TODO: enter/exit tests
+def test_enter(tmp_path_factory, testmods):
+    root = tmp_path_factory.mktemp("ctx_enter")
+
+    with open(root / "mods.json", "w") as f:
+        json.dump({"mods": testmods, "installed": ["Test Mod 2"]}, f, cls=ModEncoder)
+
+    with context(root=root, mods_dir="mods", install_dir="install") as ctx:
+        assert ctx.mods == testmods
+        assert ctx.installed == set(["Test Mod 2"])
+
+
+def test_exit(tmp_path_factory, testmods):
+    root = tmp_path_factory.mktemp("ctx_exit")
+    installed = set(["Test Mod 1"])
+
+    with context(root=root, mods_dir="mods", install_dir="install") as ctx:
+        ctx.mods = testmods
+        ctx.installed = installed
+
+    with open(root / "mods.json") as f:
+        data = json.load(f, object_hook=decode_mod)
+        assert data["mods"] == testmods
+        assert set(data["installed"]) == installed
